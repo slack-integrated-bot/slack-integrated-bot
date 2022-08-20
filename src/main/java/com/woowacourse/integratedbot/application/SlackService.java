a505f154c2b9351431c0872542135bb4c0a7a2df
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-08-21 04:18:24 +0900

    feat: SlackService 에서 발생하는 예외에 대한 try-catch 케이스 구체화

--- src/main/java/com/woowacourse/integratedbot/application/SlackService.java
+++ src/main/java/com/woowacourse/integratedbot/application/SlackService.java
@@ -1,9 +1,12 @@
 package com.woowacourse.integratedbot.application;
 
 import com.slack.api.Slack;
+import com.slack.api.methods.SlackApiException;
 import com.slack.api.methods.request.chat.ChatPostMessageRequest;
 import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 import com.woowacourse.integratedbot.application.request.SlackPostMessageRequest;
+import com.woowacourse.integratedbot.exception.ChannelNotFoundException;
+import com.woowacourse.integratedbot.exception.SlackException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -18,19 +21,25 @@ public class SlackService {
     }
 
     @Async
-    public void sendSlackMessage(final SlackPostMessageRequest request) {
+    public void sendSlackMessage(final SlackPostMessageRequest request) throws Exception {
         try (Slack slack = Slack.getInstance()) {
             ChatPostMessageRequest chatRequest = request.toChatPostMessageRequest();
             ChatPostMessageResponse response = slack.methods(slackToken)
                     .chatPostMessage(chatRequest);
 
-            if (!response.isOk()) {
-                String message = response.getError(); // "invalid_auth", "channel_not_found"
-                System.out.println(message);
-                throw new RuntimeException("");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+            checkResponseNotOk(response);
+        } catch (SlackApiException e) {
+            throw new SlackException(e.getMessage());
         }
+    }
+
+    private void checkResponseNotOk(final ChatPostMessageResponse response) {
+        if (response.isOk()) {
+            return;
+        }
+        if (response.getError().equals("channel_not_found")) {
+            throw new ChannelNotFoundException("존재하지 않는 채널입니다.");
+        }
+        throw new SlackException(response.getError());
     }
 }
---
+++ src/main/java/com/woowacourse/integratedbot/exception/SlackException.java
@@ -0,0 +1,8 @@
+package com.woowacourse.integratedbot.exception;
+
+public class SlackException extends RuntimeException {
+
+    public SlackException(final String message) {
+        super(message);
+    }
+}
